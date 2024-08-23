f4f78dcf36e66df8ec9270672b9cfb3c5d45003e
Divyu <divyendureddy@gmail.com>
2024-08-23 21:41:21 +0530

    Fix typo

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 const numberButtons = document.querySelectorAll(".button-number");
 const input = document.querySelector(".input");
 const zeroButton = document.querySelector(".button-zero");
-const inputexp = documnent.querySelector(".inputexp");
+const inputexp = document.querySelector(".inputexp");
 const operators = document.querySelectorAll(".button-operator");
 zeroButton.addEventListener("click", () => {
     input.value += zeroButton.innerHTML;
